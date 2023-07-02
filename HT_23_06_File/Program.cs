39db7541146dca2244fd4b7eb1d4097aa881c0c9
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-07-02 19:52:35 +0500

    Add project files.

---
+++ HT_23_06_File/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HT_23_06_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "text";
+            //CreateFileMethod(path);   //Firstly, we have to create files!
+            WriteToFile(path, "Nurmuhammad a'lochi bola!");
+
+            void CreateFileMethod(string path)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    File.Open(path + $"{i}.txt", FileMode.CreateNew);
+                }
+
+                Console.WriteLine("Files created!");
+            }
+
+            void WriteToFile(string path, string text)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    File.WriteAllText(path + $"{i}.txt", text);
+                }
+                Console.WriteLine("Writed to all files!");
+            }
+        }
+    }
+}
